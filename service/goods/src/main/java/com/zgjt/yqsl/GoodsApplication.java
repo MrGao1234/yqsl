b4538eb26e7f047f355a66ab7b2111641727819b
gaoxin <gx_java@163.com>
2021-08-10 15:02:37 +0800

    应用类型改成war包：1，使用tomcat部署，需要吧main方法继承 2，父pom不使用 parent 标签

--- service/goods/src/main/java/com/zgjt/yqsl/GoodsApplication.java
+++ service/goods/src/main/java/com/zgjt/yqsl/GoodsApplication.java
@@ -4,13 +4,20 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
 @MapperScan({"com.zgjt.yqsl.mapper","com.zgjt.yqsl.*.mapper"})
-public class GoodsApplication {
+public class GoodsApplication extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(GoodsApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GoodsApplication.class);
     }
 }
--- service/goods/src/main/test/com/zgjt/yqsl/component/JasyptApplicationTests.java
+++ service/goods/src/main/test/com/zgjt/yqsl/component/JasyptApplicationTests.java
@@ -3,13 +3,10 @@ package com.zgjt.yqsl.component;
 import com.zgjt.yqsl.GoodsApplication;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.DigestUtils;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = GoodsApplication.class)
 public class JasyptApplicationTests {
     @Autowired
